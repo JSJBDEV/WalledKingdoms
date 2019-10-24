c3f984d2bf4907ca89cbd0c6c65cdc3450291068
BuildTools <unconfigured@null.spigotmc.org>
2019-10-24 22:11:00 +0100

    started Citizen class

--- src/main/java/gd/rf/acro/walledkingdoms/Citizens/Citizens.java
+++ src/main/java/gd/rf/acro/walledkingdoms/Citizens/Citizens.java
@@ -1,4 +1,11 @@
 package gd.rf.acro.walledkingdoms.Citizens;
 
 public class Citizens {
+
+    public static String createCitizenString()
+    {
+        String full = "";
+
+        return full;
+    }
 }
